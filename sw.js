66eace073fc7cbefd4345f71bc181b8ce2e5d7c6
escolapontedosaberpinhais <escolapontedosaberpinhais@gmail.com>
2026-05-18 13:40:38 -0300

    fix: bump cache v5 para forcar refresh do service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ponte-saber-v3';
+const CACHE = 'ponte-saber-v5';
 const STATIC = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
