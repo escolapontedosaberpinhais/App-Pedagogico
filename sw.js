9051b13954a73c21c6faf422c86d26c844646c6d
Escola Ponte do Saber <mendesleo1986@gmail.com>
2026-06-01 00:57:58 -0300

    fix: correcoes de exclusao, sync Firestore e layout impressao semanal v2.5.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ponte-saber-v5';
+const CACHE = 'ponte-saber-v6';
 const STATIC = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
