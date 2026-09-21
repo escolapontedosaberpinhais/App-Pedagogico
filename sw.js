93e405cdf923ca9b642cff89b47bf7f732157659
mendesleo1986-droid <mendesleo1986@gmail.com>
2026-09-21 09:49:20 -0300

    Corrige colecoes trocadas no carregamento inicial (#68)
    loadFromFirestore desempacotava os documentos por posicao, e quatro
    colecoes acabaram lidas umas no lugar das outras (impressoes<->orientacoes,
    observacoes<->chatGeral). Alem de exibir dados errados, as flags de
    carregamento eram ligadas junto, entao um save() seguinte gravava o array
    trocado por cima da colecao certa no Firestore.
    
    Agora cada documento e recuperado pelo nome da colecao, nunca pela posicao.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ponte-saber-v52';
+const CACHE = 'ponte-saber-v53';
 const STATIC = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
